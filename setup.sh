fb664acfe91c2e86a88d6ed27f9ecfe310012cc4
Nooodles <brestofthebrest@gmail.com>
2025-12-15 23:36:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ cd $HOME
 apt install zsh -y
 wget https://raw.githubusercontent.com/Nooodles/public_config/refs/heads/main/.zshrc
 
+# Temporary, until I sort the line ending stuff out
+mv .zshrc .zshrc_dos
+tr -d '\r' < .zshrc_dos > .zshrc 
+rm .zshrc_dos
+
 apt install git fzf curl unzip vim -y
 curl -s https://ohmyposh.dev/install.sh | bash -s
 wget https://raw.githubusercontent.com/Nooodles/public_config/refs/heads/main/theme.omp.json
