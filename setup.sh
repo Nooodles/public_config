aeab1379e7fde65b5942d485b067cad0c3cb59e7
Nooodles <brestofthebrest@gmail.com>
2025-12-15 23:04:09 +0100

    Replace curl with wget

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 cd $HOME
 apt install zsh -y
-curl -fsSL https://raw.githubusercontent.com/Nooodles/public_config/refs/heads/main/.zshrc
+wget https://raw.githubusercontent.com/Nooodles/public_config/refs/heads/main/.zshrc
 
 apt install git fzf curl unzip -y
 curl -s https://ohmyposh.dev/install.sh | bash -s
-curl -fsSL https://raw.githubusercontent.com/Nooodles/public_config/refs/heads/main/theme.omp.json
+wget https://raw.githubusercontent.com/Nooodles/public_config/refs/heads/main/theme.omp.json
 mkdir -p ~/.config/ohmyposh/
 mv theme.omp.json ~/.config/ohmyposh/
 
